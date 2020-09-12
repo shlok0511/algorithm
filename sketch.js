a6f72f68e8105c8e1a498294bea4a17e57c9d804
shlok0511 <70267055+shlok0511@users.noreply.github.com>
2020-09-12 16:47:14 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,24 +1,45 @@
 var box1,box2;
 function setup() {
-  createCanvas(800,400);
-  box1 = createSprite(400, 200, 50, 50);
-  box2 = createSprite(200,200,50,50);
+  createCanvas(600,600);
+  box1 = createSprite(550, 550, 50, 50);
+  box2 = createSprite(50,50,50,50);
   box1.shapeColor = "blue";
   box2.shapeColor = "blue";
+  box1.velocityX = -2;
+  box1.velocityY = -2;
+  box2.velocityX = 2;
+  box2.velocityY = 2;
 }
 
 function draw() {
   background(255,255,255); 
-  box1.x = mouseX;
-  box1.y = mouseY;
-  if(box1.x-box2.x<box1.width/2+box2.width/2 && box2.x-box1.x<box1.width/2+box2.width/2
-  && box1.y-box2.y<box1.height/2+box2.height/2 && box2.y-box1.y<box1.height/2+box2.height/2 ){
+  
+  if(istouching (box1, box2)){
     box1.shapeColor = "green";
     box2.shapeColor = "yellow"; 
-  }else{
+  /*}else{
     box1.shapeColor = "blue";
-  box2.shapeColor = "blue";
+  box2.shapeColor = "blue";*/
   }
-
+ bounceoff (box1, box2);
   drawSprites();
+}
+function istouching (b1, b2){
+  if (b1.x-b2.x<b1.width/2+b2.width/2 && b2.x-b1.x<b1.width/2+b2.width/2
+    && b1.y-b2.y<b1.height/2+b2.height/2 && b2.y-b1.y<b1.height/2+b2.height/2 ){
+      return true ;
+    }else {
+      return false ;
+    }
+
+}
+function bounceoff (b1, b2){
+  if (b1.x-b2.x<b1.width/2+b2.width/2 && b2.x-b1.x<b1.width/2+b2.width/2){
+    b1.velocityX *= -1;
+    b2.velocityX *= -1;
+  }
+  if (b1.y-b2.y<b1.height/2+b2.height/2 && b2.y-b1.y<b1.height/2+b2.height/2){
+    b1.velocityY *= -1;
+    b2.velocityY *= -1;
+  }
 }
